8466f7989862df7f61a27cf8f10c17b4748294fe
Colleen Feola <colleenfeola@Colleens-MacBook-Pro.local>
2016-11-30 20:21:20 -0500

    Commenting code

--- nemo_pygame_updated.py
+++ nemo_pygame_updated.py
@@ -1,25 +1,30 @@
 
 import pygame
 
+# Intiatilizes pygame modules and sets caption for pygame window 
 pygame.init()
 
 pygame.display.set_caption('Finding Nemo')
 
+# Initializes mixer and loads sound clips to play during pygame 
 pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
 
 background_music = pygame.mixer.Sound("Just-keep-swimming-swimming-swimming_2_.wav")
 sharkbite = pygame.mixer.Sound("chomp.wav")
 find_nemo = pygame.mixer.Sound("yay.wav")
 
+# Color keys 
 black = (0,0,0)
 white = (255,255,255)
 
+# Create clock to kepe track of time 
 font = pygame.font.SysFont(None, 40)
 clock = pygame.time.Clock()
 frame_count = 0 
 frame_rate = 25
-start_time = 5
+start_time = 60
 
+# Creates class and loads, scales, and positions images for each sprite used in pygame - including Marlin, Bruce, and Nemo 
 class Marlin(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
@@ -56,31 +61,34 @@ class Dory(pygame.sprite.Sprite):
 		self.rect.x = 20
 		self.rect.y = 290
 
+# Sets the size of the pygame window to the size of the background image 
 background_img = pygame.image.load('Anemone.bmp')
 backgroundRect = background_img.get_rect()
 size = (width, height) = background_img.get_size()
 screen = pygame.display.set_mode(size)
 
+# Creates a sprite group and instances of each class to add to the sprite group 
 sprite_group = pygame.sprite.Group()
 
-# Create Marlin 
+# Creates Marlin instance
 Marlin = Marlin()
 sprite_group.add(Marlin)
 
-# Create Bruce 
+# Creates Bruce instance 
 Bruce = Bruce()
 sprite_group.add(Bruce)
 
-# Create Nemo
+# Creates Nemo instance 
 Nemo = Nemo()
 sprite_group.add(Nemo)
 
-# Create Dory 
+# Creates Dory instance 
 Dory = Dory()
 sprite_group.add(Dory)
 
 gameExit = False
 
+# Plays background music throughout the duration of the game 
 pygame.mixer.Sound.play(background_music, loops = -1)
 
 while not gameExit:
@@ -90,9 +98,12 @@ while not gameExit:
 
 	sprite_group.update()
 	
+	# When the user presses the arrow keys, the Marlin and Bruce sprites move in coordination
 	if event.type == pygame.KEYDOWN:
 		
+		# Move the sprites so long as they don't collide with each other
 		if event.key == pygame.K_LEFT and not pygame.sprite.collide_rect(Marlin,Bruce) and not pygame.sprite.collide_rect(Marlin, Nemo):
+			# Detects if the sprites go off of the screen - if they do go off of the screen, they are repositioned to their starting positions 
 			if Bruce.rect.x > width:
 				Bruce.rect.x = 500
 				Bruce.rect.y = 0
@@ -102,7 +113,7 @@ while not gameExit:
 			else:
 				Marlin.rect.x -= 10
 				Bruce.rect.x += 20
-
+			# If the Marlin and Bruce sprites collide, the game is lost. 
 			if pygame.sprite.collide_rect(Marlin,Bruce):
 				pygame.mixer.Sound.stop(background_music)
 				pygame.mixer.Sound.play(sharkbite, loops = 0)
@@ -113,6 +124,7 @@ while not gameExit:
 				pygame.display.flip()
 				print("GAME OVER")
 				exit()
+			# If the Marlin and Nemo sprites collide, the game is won. 
 			if pygame.sprite.collide_rect(Marlin, Nemo):
 				pygame.mixer.Sound.stop(background_music)
 				pygame.mixer.Sound.play(find_nemo, loops = 0)
@@ -220,14 +232,15 @@ while not gameExit:
 				print("YOU FOUND NEMO!")
 				exit()
 
-			
+	# Draws the background surface 
 	screen.blit(background_img, backgroundRect)
 	
+	# Draws the sprites on the screen surface 
 	sprite_group.draw(screen)
 	
-	# Timer
+	# Displays countdown timer 
 	total_time = start_time - (frame_count // frame_rate)
-	
+	# When time runs out, the game is over. 
 	if total_time == 0:
 		pygame.mixer.Sound.stop(background_music)
 		font = pygame.font.SysFont(None, 50)
